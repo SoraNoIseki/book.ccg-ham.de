17777c360151afb5bd9eb75a6f9c94c8f08fd436
SoraNoIseki <qyz1206@hotmail.com>
2025-03-05 14:27:34 +0100

    fix: base url errors

--- packages/soranoiseki/book-group/src/Resources/js/task-plan/services/routes.ts
+++ packages/soranoiseki/book-group/src/Resources/js/task-plan/services/routes.ts
@@ -1,19 +1,17 @@
-const baseUrl = "/api/task-plan";
-
 const routes = {
     plan: {
-        index: baseUrl + "/plans",
-        update: baseUrl + "/plans",
+        index: "/plans",
+        update: "/plans",
     },
     member: {
-        index: baseUrl + "/members",
-        delete: baseUrl + "/members/delete",
-        update: baseUrl + "/members/update",
-        create: baseUrl + "/members/create",
+        index: "/members",
+        delete: "/members/delete",
+        update: "/members/update",
+        create: "/members/create",
     },
     group: {
-        index: baseUrl + "/groups",
-        toggle: baseUrl + "/groups/roles/toggle",
+        index: "/groups",
+        toggle: "/groups/roles/toggle",
     },
     
 };
